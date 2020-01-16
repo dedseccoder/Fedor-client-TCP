aebfb436aa82ab1b026a4b6e7a7ba02f9f445292
dedseccoder <andrey.flach2@gmail.com>
2020-01-16 04:45:06 +0300

    fixed comments in code

--- FEDOR client/FEDOR client/FEDOR client.cpp
+++ FEDOR client/FEDOR client/FEDOR client.cpp
@@ -8,27 +8,28 @@ using namespace std;
 
 int main()
 {
-	// выбор верссии сокет библиотеки
+	// choose lib version
 	WSAData wsaData;
 	WORD lib_version = MAKEWORD(2, 1);
-	//проверка успешности инициализации библиотеки 
+	//lib installation check 
 	if (WSAStartup(lib_version, &wsaData) != 0)
 	{
 		cout << "WSAStartup ERROR" << endl;
+		system("pause");
 		exit(1);
 	}
-	// ¬вод айпи дл€ любого компа
+	// Enter user's IP for not only one machine
 	char IP [20];
-	cout << "input your local IP adress" << endl;
+	cout << "input your local IP address ";
 	cin >> IP;
-	// SOCKADDR_IN хранит айпи адресс пользовател€
+	// SOCKADDR_IN gets user's IP address
 	SOCKADDR_IN addr;
 	addr.sin_addr.s_addr = inet_addr(IP);
 	addr.sin_port = htons(10099);
 	addr.sin_family = AF_INET;
-	//создание сокета дл€ коннекта с сервером
+	//creat socket for connetion to server
 	SOCKET operator_socket = socket(AF_INET, SOCK_STREAM, NULL);
-	//коннект к серверу с проверкой
+	//connection to server with check
 	if (connect(operator_socket, (SOCKADDR*)&addr, sizeof(addr)) != 0)
 	{
 		cout << "Connection ERROR" << endl;
